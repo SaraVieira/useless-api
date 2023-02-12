71426794607c56694c9f06822ead0b3610a5c4a2
Sara Vieira <hey@iamsaravieira.com>
2023-02-12 17:34:25 +0000

    maybe

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,3 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
-
 export default function Home() {
-  return (
-   'sup'
-  )
+  return "sup";
 }
